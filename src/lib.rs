8f1e5f0de1240cafdafdf31ef8598e74f932fee8
flowerinthenight <root@flowerinthenight.com>
2024-12-03 14:31:08 +0900

    add lib

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+pub struct MyStruct {}
+
+impl MyStruct {
+    pub fn hello(&self) -> Result<usize, io::Error> {
+        Ok(100)
+    }
+}
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use spindle_rs::*;
+
 fn main() {
-    println!("Hello, world!");
+    let result = add(1, 2);
+    let ms = MyStruct {};
+    println!("Hello, world! {}, {}", result, ms.hello().unwrap());
+    println!("{}", if true { 6 } else { 7 });
 }
