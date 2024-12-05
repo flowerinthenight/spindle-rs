635ac2eec63c11706009f5be3902f3686d5687d2
flowerinthenight <root@flowerinthenight.com>
2024-12-05 10:16:51 +0900

    try parsing spanner

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,78 @@
+use google_cloud_rust_raw::spanner::v1::{
+    spanner::{CreateSessionRequest, ExecuteSqlRequest, Session},
+    spanner_grpc::SpannerClient,
+};
+use grpcio::{CallOption, ChannelBuilder, ChannelCredentials, EnvBuilder, MetadataBuilder};
+use protobuf::well_known_types::Value_oneof_kind;
 use std::io;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
 
 pub struct Lock {
     name: String,
+    client: SpannerClient,
+    session: Session,
+    active: Arc<AtomicUsize>,
+    timeout: Option<u64>,
 }
 
 impl Lock {
-    pub fn new(n: String) -> Self {
-        Self { name: n }
+    pub fn new(name: String, db: String) -> Self {
+        let endpoint = "spanner.googleapis.com:443";
+        let env = Arc::new(EnvBuilder::new().build());
+        let creds = ChannelCredentials::google_default_credentials().unwrap();
+
+        // Create Spanner client.
+        let chan = ChannelBuilder::new(env.clone())
+            .max_send_message_len(100 << 20)
+            .max_receive_message_len(100 << 20)
+            .set_credentials(creds)
+            .connect(&endpoint);
+        let client = SpannerClient::new(chan);
+
+        // Connect to the instance and create a Spanner session.
+        let mut req = CreateSessionRequest::new();
+        req.database = db.to_string();
+        let mut meta = MetadataBuilder::new();
+        meta.add_str("google-cloud-resource-prefix", db.as_str())
+            .unwrap();
+        meta.add_str("x-goog-api-client", "googleapis-rs").unwrap();
+        let opt = CallOption::default().headers(meta.build());
+        let session = client.create_session_opt(&req, opt).unwrap();
+
+        Self {
+            name,
+            client,
+            session,
+            active: Arc::new(AtomicUsize::new(0)),
+            timeout: Some(2000),
+        }
     }
 
-    pub fn hello(&self) {
-        println!("{}", &self.name);
+    pub fn query(&self) {
+        println!("name={}", self.name);
+        let mut req = ExecuteSqlRequest::new();
+        req.session = self.session.get_name().to_string();
+        req.sql = "select * from alerts".to_string();
+        let out = self.client.execute_sql(&req).unwrap();
+        for i in out.get_rows() {
+            let v0 = i.get_values().get(0);
+            match &v0.unwrap_or_default().kind {
+                Some(v) => match v {
+                    Value_oneof_kind::string_value(v) => {
+                        println!("val={}", v)
+                    }
+                    _ => {}
+                },
+                _ => {}
+            }
+        }
+    }
+
+    pub fn inc(&self) {
+        let v = Arc::clone(&self.active);
+        println!("atomic={}", v.fetch_add(1, Ordering::SeqCst));
+        println!("timeout={}", &self.timeout.unwrap_or_else(|| 5000));
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,23 @@
-use google_cloud_rust_raw::spanner::v1::{
-    spanner::{CreateSessionRequest, ExecuteSqlRequest},
-    spanner_grpc::SpannerClient,
-};
-use grpcio::{CallOption, ChannelBuilder, ChannelCredentials, EnvBuilder, MetadataBuilder};
 use spindle_rs::*;
 use std::env;
 use std::error::Error;
 use std::sync::mpsc;
-use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
+    println!("args[0]={:?}", args.get(1));
 
-    let a0 = args.get(1);
-    println!("0 = {:?}", a0);
+    let lock = Lock::new(
+        "spindle".to_string(),
+        "projects/mobingi-main/instances/alphaus-prod/databases/main".to_string(),
+    );
 
-    let lock = Lock::new("spindle".to_string());
-    lock.hello();
-
-    let result = add(1, 2);
-    let ms = MyStruct {};
-    println!("Hello, world! {}, {}", result, ms.hello().unwrap());
-    println!("{}", if true { 6 } else { 7 });
-
-    // let thr = thread::spawn(|| loop {
-    //     println!("from the spawned thread!");
-    //     thread::sleep(Duration::from_millis(2000));
-    // });
-
-    // thr.join().unwrap();
+    lock.query();
+    lock.query();
+    lock.inc();
+    lock.inc();
 
     let (tx, rx) = mpsc::channel();
 
@@ -46,7 +32,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         for val in vals {
             tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
+            thread::sleep(Duration::from_millis(100));
         }
     });
 
@@ -55,35 +41,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     thr.join().unwrap();
-
-    let db = "projects/mobingi-main/instances/alphaus-prod/databases/main";
-    let endpoint = "spanner.googleapis.com:443";
-    let env = Arc::new(EnvBuilder::new().build());
-    let creds = ChannelCredentials::google_default_credentials().unwrap();
-
-    // Create a Spanner client.
-    let chan = ChannelBuilder::new(env.clone())
-        .max_send_message_len(100 << 20)
-        .max_receive_message_len(100 << 20)
-        .set_credentials(creds)
-        .connect(&endpoint);
-    let client = SpannerClient::new(chan);
-
-    // Connect to the instance and create a Spanner session.
-    let mut req = CreateSessionRequest::new();
-    req.database = db.to_string();
-    let mut meta = MetadataBuilder::new();
-    meta.add_str("google-cloud-resource-prefix", db).unwrap();
-    meta.add_str("x-goog-api-client", "googleapis-rs").unwrap();
-    let opt = CallOption::default().headers(meta.build());
-    let session = client.create_session_opt(&req, opt).unwrap();
-
-    // Prepare a SQL command to execute.
-    let mut req = ExecuteSqlRequest::new();
-    req.session = session.get_name().to_string();
-    req.sql = "select * from alerts".to_string();
-    let out = client.execute_sql(&req).unwrap();
-    dbg!(out);
 
     Ok(())
 }
